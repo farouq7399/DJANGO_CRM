cbb58e9670380dad6f4aec992d53b5838e0f1993
OmarElmahdy7399 <73123128+OmarElmahdy7399@users.noreply.github.com>
2022-02-21 07:07:05 +0200

    added function base views error in create check button delete for update

--- CRM_DJANGO/leads/urls.py
+++ CRM_DJANGO/leads/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('leads/', views.home_page, name="home_page"),
-    path('leads/lead_create/', views.lead_create, name="create_lead"),
-    path('leads/lead_detail/<int:pk>', views.lead_detail, name="lead_details"),
+    #path('leads/', views.home_page, name="home_page"),
+    #path('leads/lead_create/', views.lead_create, name="create_lead"),
+    #path('leads/lead_detail/<int:pk>', views.lead_detail, name="lead_details"),
     path('leads/lead_update/<int:pk>', views.lead_update, name="update_lead"),
     path('leads/lead_delete/<int:pk>', views.lead_delete, name="delete_lead"),
+    # ===== This is start of the Class-Based Views
+    path('leads/', views.LeadListView.as_view(), name="home_page"),
+    path('leads/details/<int:pk>', views.LeadDetailsView.as_view(), name="lead_details"),
+    path('leads/lead_create/', views.LeadCreateView.as_view(), name="create_lead"),
+
 ]
