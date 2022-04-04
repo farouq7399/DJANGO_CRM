bd00ee3afd426753d2a891551577124c28e54804
OmarElmahdy7399 <73123128+OmarElmahdy7399@users.noreply.github.com>
2022-04-04 17:02:38 +0200

    -adjusted delete function :still have bugs to delete -added landing page view and template -still working on rest permission and authentications

--- CRM_DJANGO/agents/urls.py
+++ CRM_DJANGO/agents/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import AgentListView, AgentCreateView
+from .views import AgentListView, AgentCreateView, AgentDetailView
 
 
 app_name = 'agents'
 
 urlpatterns = [
     path("Agent_list/", AgentListView.as_view(), name="agent_list"),
-    path("Agent_create/", AgentCreateView.as_view(), name="agent_create")
+    path("Agent_create/", AgentCreateView.as_view(), name="agent_create"),
+    path("Agent_details/<int:pk>", AgentDetailView.as_view(), name="agent_details"),
 ]
